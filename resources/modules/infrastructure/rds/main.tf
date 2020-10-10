bb231c2ab3948214f30afc4526182140e2bb0215
Chinenye Nwandu <67292767+Chinenye-VN@users.noreply.github.com>
2020-10-10 23:56:02 +0100

    Update main.tf

--- resources/modules/infrastructure/rds/main.tf
+++ resources/modules/infrastructure/rds/main.tf
@@ -1,1 +1,9 @@
-
+resource "aws_db_instance" "test database" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
