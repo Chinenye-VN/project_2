876e83067c75b73b24ebd20a0871b45b23172b1b
Chinenye-VN <veronica.nwandu@gmail.com>
2020-10-10 23:36:45 +0100

    added main and variables file

---
+++ resources/modules/infrastructure/ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "public_instance" {
+  ami                         = "${var.ami_id}"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${var.public_subnet_id}"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.environment}-${var.region}-Public"
+  }
+}
