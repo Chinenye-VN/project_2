27088cc8fd5f78d4c33f29ff529a4698760daee9
Chinenye Nwandu <67292767+Chinenye-VN@users.noreply.github.com>
2020-10-11 18:46:52 +0100

    Update variables.tf

--- resources/modules/infrastructure/ec2/variables.tf
+++ resources/modules/infrastructure/ec2/variables.tf
@@ -1,8 +1,35 @@
-variable "environment" {
+variable "ami" {
+  description = "AMI of EC2 instance, default is Ubuntu 18.04"
+  default     = "ami-09a1e275e350acf38"
 }
-variable "region" {
+
+variable "instance-type" {
+  description = "The type of machine to run the image on"
+  default     = "t2.micro"
 }
-variable "ami_id" {
+
+variable "pem-key" {
+  description = "The Key which I will be able to SSH in to my EC2 using"
+  default     = "MyKey"
 }
-variable "public_subnet_id" {
+
+variable "subnet_id" {
+  description = "The ID of the subnet which I will deploy EC2 in to"
+  default     = "4"
+}
+
+variable "vpc_security_group_ids" {
+  description = "The IDs of the security group we will attach to the EC2"
+  default     = "2"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Map of tags"
+  default     = { Name : "Jenkins", Project : "Project_2" }
+}
+
+variable "associate_public_ip_address" {
+  description = "Associate Public IP to machines that need public access"
+  default     = false
 }
