66178e34f9d7a76c26e165d117f38255851fff1e
Chinenye Nwandu <67292767+Chinenye-VN@users.noreply.github.com>
2020-10-11 18:49:53 +0100

    Update and rename resources/modules/infrastructure/subnets/main.tf to resources/modules/infrastructure/securitygroups/main.tf

--- resources/modules/infrastructure/subnets/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = "${var.vpc_id}"
-  cidr_block = "10.0.0.0/24"
-
-  tags = {
-    Name = "${var.environment}-${var.region}-Public-Subnet"
-  }
-}
