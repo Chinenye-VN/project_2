84ae4f19364e3167ffe2d2a48ad8ee1712d9c806
Chinenye Nwandu <67292767+Chinenye-VN@users.noreply.github.com>
2020-10-11 21:10:24 +0100

    Update main.tf

--- resources/modules/infrastructure/main.tf
+++ resources/modules/infrastructure/main.tf
@@ -1,12 +1,6 @@
 module "ec2" {
   source = "./ec2"
-
-  region           = var.region
-  ami_id           = var.ami_id
 }
 module "rds" {
   source = "./rds"
-
-  region      = var.region
-  vpc_id      = module.vpc.vpc_id
 }
