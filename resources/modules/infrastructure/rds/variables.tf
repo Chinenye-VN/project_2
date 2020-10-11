1c43801af0c4562c921240e18ee567e16c6e0ace
Chinenye Nwandu <67292767+Chinenye-VN@users.noreply.github.com>
2020-10-11 19:38:25 +0100

    Update variables.tf

--- resources/modules/infrastructure/rds/variables.tf
+++ resources/modules/infrastructure/rds/variables.tf
@@ -1,1 +1,29 @@
+variable "engine" {
+  description = "The type of database"
+  default     = "mysql"
+}
+variable "engine_version" {
+  description = "The version mysql database"
+  default     = "5.7"
+}
+variable "instance_class" {
+  description = "The type of instance the database will be using"
+  default     = "db.t2.micro"
+}
+variable "name" {
+  description = "The name of this RDS"
+  default     = "mydb"
+}
+variable "username" {
+  description = "The username to access mysql"
+  default     = "admin"
+}
+variable "password" {
+  description = "The password to access mysql"
+  default     = "mysql"
+}
+variable "vpc_security_groups_ids" {
+  description = "The IDs of the security group we will attach to the RDS"
+}
+
 
