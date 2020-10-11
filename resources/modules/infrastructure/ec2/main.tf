b0a04387a1d9ab9ed81269f65d3aeba68bffe930
Chinenye Nwandu <67292767+Chinenye-VN@users.noreply.github.com>
2020-10-12 00:41:30 +0100

    Update main.tf

--- resources/modules/infrastructure/ec2/main.tf
+++ resources/modules/infrastructure/ec2/main.tf
@@ -3,13 +3,10 @@ resource "aws_instance" "Instances" {
   instance_type          = var.instance-type
   key_name               = var.pem-key
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.vpc_security_group_ids]
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   lifecycle {
     create_before_destroy = true
-  }
-  tags = {
-    name = var.tags
 
   }
   associate_public_ip_address = var.associate_public_ip_address
