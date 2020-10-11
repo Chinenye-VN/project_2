1852486a357ad5c17800b3e780a0170c93978e87
Chinenye Nwandu <67292767+Chinenye-VN@users.noreply.github.com>
2020-10-11 18:39:29 +0100

    Update main.tf

--- resources/modules/infrastructure/ec2/main.tf
+++ resources/modules/infrastructure/ec2/main.tf
@@ -1,13 +1,3 @@
-resource "aws_instance" "public_instance" {
-  ami                         = "${var.ami_id}"
-  instance_type               = "t2.micro"
-  subnet_id                   = "${var.public_subnet_id}"
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "${var.environment}-${var.region}-Public"
-  }
-}
 resource "aws_instance" "Instances" {
   ami                    = var.ami
   instance_type          = var.instance-type
